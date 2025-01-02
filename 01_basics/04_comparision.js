8a3dcc6120e5aafb9088e37884fd46bddc4183f0
Abhishek44554455 <sauravk8825@gmail.com>
2025-01-02 10:05:55 +0000

    compaision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,14 @@
+// console.log("2">3)
+// console.log("2"<3)
+
+
+// console.log(null>0)
+// console.log(null==0)  //the reason is that equality and comparision checks work differently, comparision converting null to zero 
+// console.log(null>=0)  //thats why >= is true but ==false
+
+
+// console.log(undefined == 0)
+// console.log(undefined>=0)
+
+//   ===   // strict check
+console.log("2"===2)  //it checks both datatype and value
