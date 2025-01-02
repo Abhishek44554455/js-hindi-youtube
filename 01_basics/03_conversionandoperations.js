30a5f76d4b9c13160b5fd09a73f16840e8950f28
Abhishek44554455 <sauravk8825@gmail.com>
2025-01-02 09:23:35 +0000

    conversion

---
+++ 01_basics/03_conversionandoperations.js
@@ -0,0 +1,15 @@
+// console.log(2+2)
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(5/2);
+// console.log(2%3);
+
+
+let str1="hello "
+let str2="Abhishek"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2+2)
+console.log(1+2+"2")
