06614f025d32798b9162824b739fa9c628b4f1ed
Abhishek44554455 <sauravk8825@gmail.com>
2025-01-02 07:37:10 +0000

    datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,21 @@
+const accountId=144253
+let accountEmail="LookAtme@gmail.com"
+var accountPassword="12345678"
+acountCity="Jaipur"
+
+accountEmail="Lookatyou@gmail.com"
+accountPassword="87654321"
+acountCity="Bihar"
+
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(acountCity)
+console.table([accountId,accountEmail,accountPassword,acountCity])
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
+
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+//"Use Strict;"// Treat all js code as newer version
+
+//alert(3+3)//not using because we dont use browser
+    
+let name="Abhishek"
+let age=19
+let isLoggedIn=false
+
+
+
+//number=>2 to power 53
+//bigint
+//string=""
+//boolean
+//null=> standard value
+//undefined
+//symbol
+
+
+console.log(typeof undefined);
+console.log(typeof null);
+
